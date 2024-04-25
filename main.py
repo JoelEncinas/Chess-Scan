809cc5af5e312121778295789bc8c85ed2235ff0
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-25 03:21:31 +0200

    print a chessboard with the piece into console

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cv2
 
 # load and grayscale images
 image = cv2.imread('./images/positions/test-2.png')
-template = cv2.imread('./images/pieces/classic/wq.png')
+template = cv2.imread('./images/pieces/classic/bk.png')
 
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
@@ -40,6 +40,26 @@ x1, y1 = max_loc
 x2, y2 = max_loc[0] + resized_template.shape[1], max_loc[1] + resized_template.shape[0]
 
 cv2.rectangle(resized_roi, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+# Initialize array to store positions
+positions = [['empty' for _ in range(8)] for _ in range(8)]
+
+# Calculate the row and column indices
+row_index1 = y1 // 50
+col_index1 = x1 // 50
+
+row_index2 = y2 // 50
+col_index2 = x2 // 50
+
+# Add the position to the chessboard
+for i in range(row_index1, row_index2):
+    for j in range(col_index1, col_index2):
+        positions[i][j] = 'piece'
+
+# Print the updated positions
+print("Chessboard")
+for row in positions:
+    print(row)
 
 # print image
 cv2.imshow("Chess", resized_roi)
