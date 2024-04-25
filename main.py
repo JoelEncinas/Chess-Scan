a8c0991716c4f4de26c57efcfcbe882d5cceb3ef
JoelEncinas <joelencinasmartin@gmail.com>
2024-04-25 03:08:04 +0200

    add comments

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import cv2
 # url for pieces
 # https://images.chesscomfiles.com/chess-themes/pieces/classic/150/wp.png
 
+# load and grayscale images
 image = cv2.imread('./images/positions/test-2.png')
 template = cv2.imread('./images/pieces/classic/wq.png')
 
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 template_gray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 
+# find chessboard
 _, thresh = cv2.threshold(image_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -39,6 +41,7 @@ x2, y2 = max_loc[0] + resized_template.shape[1], max_loc[1] + resized_template.s
 
 cv2.rectangle(resized_roi, (x1, y1), (x2, y2), (0, 255, 0), 3)
 
+# print image
 cv2.imshow("Chess", resized_roi)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
