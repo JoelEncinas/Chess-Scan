9d3b97e84667ee54f18931eaa665adb8a9bc52a0
Joel Encinas <joelencinasmartin@gmail.com>
2024-04-27 14:21:21 +0200

    Update views.py

--- home/views.py
+++ home/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'home/index.html', {})
+    return render(request, 'home/index.html', {}
+
+def error_404(request, exception):
+    return render(request, '404.html', {}, status=404)
