1cf8ebf06856f4d22c889a059a6ddc7a2a59ba55
JoelEncinas <joelencinasmartin@gmail.com>
2024-05-04 20:13:43 +0200

    created app again

--- chessapp/asgi.py
+++ chessapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for chessscan project.
+ASGI config for chessapp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessscan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessapp.settings')
 
 application = get_asgi_application()
--- chessapp/wsgi.py
+++ chessapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chessscan project.
+WSGI config for chessapp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessscan.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chessapp.settings')
 
 application = get_wsgi_application()
