20c161989545888da5de3d4012859b24246f6afd
JoelEncinas <joelencinasmartin@gmail.com>
2024-05-07 00:01:55 +0200

    fix static folder

--- chessapp/settings.py
+++ chessapp/settings.py
@@ -84,13 +84,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# The Following code for production may break development mode, so we need to check if we're in DEBUG mode
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on the WhiteNoise storage backend which compresses static files for smaller disk use,
-    # and renames the files with unique names for each version of that file for long term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
