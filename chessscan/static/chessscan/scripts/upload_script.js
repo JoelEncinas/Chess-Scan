7d2a04b60f3a4a9ed967e34fed3e52d71281dfab
JoelEncinas <joelencinasmartin@gmail.com>
2024-05-06 11:46:40 +0200

    style navbar and form

--- chessscan/static/chessscan/scripts/upload_script.js
+++ chessscan/static/chessscan/scripts/upload_script.js
@@ -1,20 +1,26 @@
-document.getElementById('uploadForm').addEventListener('submit', function(event) {
+document
+  .getElementById("uploadForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
     var formData = new FormData();
-    var imageFile = document.getElementById('imageInput').files[0];
-    formData.append('image', imageFile);
+    var imageFile = document.getElementById("imageInput").files[0];
+    formData.append("image", imageFile);
 
     console.log("script working");
 
-    fetch('/upload_image/', {
-        method: 'POST',
-        body: formData
+    fetch("/upload_image/", {
+      method: "POST",
+      body: formData,
     })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('result').innerText = data.message;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        document.getElementById("result").innerText = data.message;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+
+document.getElementById("imageInput").addEventListener("change", function () {
+  document.getElementById("file-chosen").textContent = this.files[0].name;
 });
